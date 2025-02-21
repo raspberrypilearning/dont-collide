6cfed4b8bb3ccccb87624bcbfef641f0d677e140
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2025-02-21 15:49:03 +0000

    New translations main.py (French)

--- fr-FR/code/dont-collide-starter/main.py
+++ fr-FR/code/dont-collide-starter/main.py
@@ -1,17 +1,25 @@
-#!/bin/python3
-
 from p5 import *
 from random import randint, seed
 
 # Mets les variables globales ici
+screen_size = 400
+
+# Draw player function goes here
+
+
+
+# Draw obstacles function goes here
 
 
 def setup():
     # Mets le code à exécuter au démarrage ici
+    size(screen_size, screen_size)
+    text_size(40)
 
 
 def draw():
     # Mets le code à exécuter à chaque image ici
+    
 
   
 # Garde ceci pour exécuter ton code
