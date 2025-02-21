745987929f2ba5e1c2b4d07107a684cb5c8f90cf
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2025-02-21 15:49:06 +0000

    New translations main.py (Italian)

--- it-IT/code/dont-collide-starter/main.py
+++ it-IT/code/dont-collide-starter/main.py
@@ -1,17 +1,25 @@
-#!/bin/python3
-
 from p5 import *
 from random import randint, seed
 
 # Includi qui le variabili globali
+screen_size = 400
+
+# Draw player function goes here
+
+
+
+# Draw obstacles function goes here
 
 
 def setup():
     # Metti qui sotto il codice che verrà eseguito una sola volta
+    size(screen_size, screen_size)
+    text_size(40)
 
 
 def draw():
     # Metti qui sotto il codice che verrà eseguito ad ogni cambio di frame
+    
 
   
 # Conserva questa parte per eseguire il codice
